f818249ddcabb5dd9c330c5c33d314305042d57d
YONGMINJO1 <iy0594@naver.com>
2025-11-14 15:56:50 +0900

    feat: CalculatorApplication 구현 완료
    - Application -> CalculatorApplication 변경
    - 다른 Application이랑 구분을 위해 변경함
    - main() 메서드에서 Controller 실행

--- src/main/java/Application.java
+++
@@ -1,5 +0,0 @@
-public class Application {
-    public static void main(String[] args) {
-        System.out.println("우테코 프리코스 오픈 미션");
-    }
-}
--- src/test/java/calculator/CalculatorApplicationTest.java
+++ src/test/java/calculator/CalculatorApplicationTest.java
@@ -1,3 +1,5 @@
+package calculator;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -5,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
-public class ApplicationTest extends NsTest {
+public class CalculatorApplicationTest extends NsTest {
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -24,6 +26,6 @@ public class ApplicationTest extends NsTest {
 
     @Override
     public void runMain() {
-        Application.main(new String[]{});
+        CalculatorApplication.main(new String[]{});
     }
 }
