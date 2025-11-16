d2b641e3ec1c82d2b1c1b342e8c53efec1ccb703
YONGMINJO1 <iy0594@naver.com>
2025-11-17 00:23:04 +0900

    feat: Lotto 크기 검증 구현
    - validate() 메서드 추가
    - validateSize() 구현
    - 숫자가 6개가 아닐 시 예외 발생
    - LOTTO_SIZE 상수 추출
    - 크기 검증 테스트 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,10 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
+    private static final int LOTTO_SIZE = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        validate(numbers);
         this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+        }
     }
 
     public List<Integer> getNumbers() {
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -17,5 +18,17 @@ public class LottoTest {
 
         //then
         assertThat(lotto.getNumbers()).hasSize(6);
+    }
+
+    @Test
+    void 로또번호는_6개여야_한다() {
+        //given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        //when & then
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("6개");
+
     }
 }
