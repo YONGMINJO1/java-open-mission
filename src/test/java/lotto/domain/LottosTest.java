17b1b22181fa1c564b48d4502c5dfd1c58c2ac55
YONGMINJO1 <iy0594@naver.com>
2025-11-17 14:23:02 +0900

    feat: Lottos 일급 컬렉션 기본 구현
    - Lottos 클래스 생성
    - List<Lotto> 저장
    - size() 메서드 추가
    - LottosTest 추가

---
+++ src/test/java/lotto/domain/LottosTest.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class LottosTest {
+
+    @Test
+    void 로또_여러_장을_생성할_수_있다() {
+        //given
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(List.of(7, 8, 9, 10, 11, 12));
+        List<Lotto> lottoList = List.of(lotto1, lotto2);
+
+        //when
+        Lottos lottos = new Lottos(lottoList);
+
+        //then
+        assertThat(lottos.size()).isEqualTo(2);
+    }
+}
