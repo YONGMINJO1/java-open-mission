23fc00a03cab901fd63afdb051e72fbbfc0fc84d
YONGMINJO1 <iy0594@naver.com>
2025-11-17 14:29:28 +0900

    feat: Lottos getLottos() 메서드 구현
    - getLottos() 메서드 추가
    - 방어적 복사
    - 로또 목록 조회 테스트 추가

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -20,4 +20,19 @@ public class LottosTest {
         //then
         assertThat(lottos.size()).isEqualTo(2);
     }
+
+    @Test
+    void 로또_목록을_조회할_수_있다() {
+        //given
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(List.of(7, 8, 9, 10, 11, 12));
+        List<Lotto> lottoList = List.of(lotto1, lotto2);
+
+        //when
+        Lottos lottos = new Lottos(lottoList);
+
+        //then
+        assertThat(lottos.getLottos()).hasSize(2);
+        assertThat(lottos.getLottos()).contains(lotto1, lotto2);
+    }
 }
