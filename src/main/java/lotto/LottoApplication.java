ff79071c4e3ee5bd51ec916e42e1712eb8d5dae7
YONGMINJO1 <iy0594@naver.com>
2025-11-17 23:53:34 +0900

    feat: LottoApplication 실행 구현
    - LottoApplication 클래스 생성
    - main() 메서드 구현
    - InputView, OutputView, LottoGenerator 객체 생성
    - LottoController에 의존성 주입
    - controller.run()으로 게임 시작

---
+++ src/main/java/lotto/LottoApplication.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import lotto.controller.LottoController;
+import lotto.domain.LottoGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoApplication {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoGenerator lottoGenerator = new LottoGenerator();
+
+        LottoController controller = new LottoController(inputView, outputView, lottoGenerator);
+        controller.run();
+    }
+}
