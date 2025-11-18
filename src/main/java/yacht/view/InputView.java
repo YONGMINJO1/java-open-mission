4413c9fc2381e7da2bfb720920cb60db6873782f
YONGMINJO1 <iy0594@naver.com>
2025-11-19 00:59:11 +0900

    feat: 카테고리 번호 입력 기능 구현
    - readCategoryNumber() 메서드 추가
    - 사용자로부터 숫자 입력 받기

--- src/main/java/yacht/view/InputView.java
+++ src/main/java/yacht/view/InputView.java
@@ -9,4 +9,11 @@ public class InputView {
         System.out.println("주사위를 굴리려면 Enter를 눌러주세요.");
         Console.readLine();
     }
+
+    public static int readCategoryNumber() {
+        System.out.println();
+        System.out.print("선택할 카테고리 번호를 입력하세요: ");
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
 }
