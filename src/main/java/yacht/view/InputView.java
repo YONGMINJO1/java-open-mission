56c6f6142dc0023ca89708028f373d90dcbe7ed7
YONGMINJO1 <iy0594@naver.com>
2025-11-19 00:54:52 +0900

    feat: InputView 기본 구조 및 throwDice 기능 구현
    - InputView 클래스 생성
    - throwDice() 메서드 추가
    - Console.readLine()을 활용한 입력 대기

---
+++ src/main/java/yacht/view/InputView.java
@@ -0,0 +1,12 @@
+package yacht.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static void throwDice() {
+        System.out.println();
+        System.out.println("주사위를 굴리려면 Enter를 눌러주세요.");
+        Console.readLine();
+    }
+}
