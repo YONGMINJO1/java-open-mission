5a5f4f439bde96f2890d87c4b5f1db72b8b2e917
YONGMINJO1 <iy0594@naver.com>
2025-11-19 00:33:04 +0900

    feat: 주사위 결과 출력 기능 구현
    - printDices() 메서드 추가
    - 주사위 번호와 값을 함께 출력
    - StringBuilder를 활용한 문자열 조합

--- src/main/java/yacht/view/OutputView.java
+++ src/main/java/yacht/view/OutputView.java
@@ -1,8 +1,25 @@
 package yacht.view;
 
+import java.util.List;
+
 public class OutputView {
 
     public static void printGameStart() {
         System.out.println("게임을 시작합니다.");
+    }
+
+    public static void printDices(List<Integer> values) {
+        System.out.println();
+        System.out.println("주사위를 굴립니다.");
+
+        StringBuilder result = new StringBuilder("[");
+        for (int i = 0; i < values.size(); i++) {
+            if (i > 0) {
+                result.append(", ");
+            }
+            result.append((i + 1) + "번: " + values.get(i));
+        }
+        result.append("]");
+        System.out.println(result.toString());
     }
 }
